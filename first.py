ac752fcdf9c7f3464e5d7fc3431c925fd18fa38e
Tejas Shah <tejasshah2k19@gmail.com>
2026-07-27 19:24:04 +0530

    init

---
+++ first.py
@@ -0,0 +1,45 @@
+# print("Welcome....first program.......")
+# print("second line")
+
+
+# print("Welcome....first program.......",end="\n")
+# print("second line",end="\n")
+
+
+
+print("Welcome....first program.......",end="###")
+print("second line",end=" ")
+
+
+print("Welcome....first program.......",end="\n")
+print("second line",end="\n")
+
+print("royal","education","ahmedabad")
+
+
+
+#single line 
+
+''' 
+multi
+   line 
+'''
+
+"""
+    this is 
+        also 
+            mutliline 
+"""
+
+
+#addition of two numbers 
+#variable 
+#datatype 
+
+
+no1 = 10  
+no2 = 20  
+
+ans = no1+no2
+
+print("Addition = ",ans)
---
+++ inputDemo.py
@@ -0,0 +1,17 @@
+print("Enter two numbers")
+no1 = int(input()) #5 -> string 
+no2 = input() #2 -> string 
+no2 = int(no2)
+
+ans = no1 + no2 
+print("addition = ",ans) # int()
+
+ans = no1 / no2 
+print("div = ",ans) # 2.5
+
+
+ans = no1 // no2 
+print("div = ",ans) # 2
+
+
+#int() float() 
